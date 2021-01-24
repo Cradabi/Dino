bf5688bbfc4366bae60476007b75d89a37c8d1d7
w-o-p <w-o-p@users.noreply.github.com>
2021-01-25 01:40:56 +0300

    small fixes

--- game_cycle.py
+++ game_cycle.py
@@ -13,7 +13,7 @@ color = 'white'
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 screen.fill(color)
-score = 1500
+score = 0
 con = sqlite3.connect('HI.db')
 cur = con.cursor()
 HI_s = cur.execute(
