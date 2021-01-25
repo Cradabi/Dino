a3e0619729b0afdc7d41c89c09777943f3749197
Daniel <75375225+Cradabi@users.noreply.github.com>
2021-01-26 00:35:15 +0300

    Update game_cycle.py

--- game_cycle.py
+++ game_cycle.py
@@ -648,7 +648,153 @@ def cut_scen_1(screen, color, score, HI, road_cord_x1):
         elif t >= 1430:
             running = False
             origin_dino(screen, color, score, HI)
-            
+
+        score_out = []
+        for i in range(1, len(score_str) + 1):
+            score_out.insert(0, score_str[len(score_str) - i])
+        while len(score_out) != 5:
+            score_out.insert(0, '0')
+        score_cord_x = 970
+        for i in range(len(score_out)):
+            screen.blit(nums_dict[score_out[i]], (score_cord_x, score_cord_y))
+            score_cord_x += 30
+
+        # преобразует лучший счет в поверхность и выводит ее:
+        score_out = []
+        HI_coard_y = 500
+        screen.blit(HI_img, (870, 500))
+        for i in range(1, len(HI_str) + 1):
+            score_out.insert(0, HI_str[len(HI_str) - i])
+        while len(score_out) != 5:
+            score_out.insert(0, '0')
+        HI_coard_x = 970
+        for i in range(len(score_out)):
+            screen.blit(nums_dict[score_out[i]], (HI_coard_x, HI_coard_y))
+            HI_coard_x += 30
+        pygame.display.flip()
+
+
+def cut_scen_2(screen, color, score, HI, road_cord_x1):
+    clock = pygame.time.Clock()
+
+    num_0 = pygame.image.load('imgs/0.png')
+    num_0.set_colorkey('white')
+    num_1 = pygame.image.load('imgs/1.png')
+    num_1.set_colorkey('white')
+    num_2 = pygame.image.load('imgs/2.png')
+    num_2.set_colorkey('white')
+    num_3 = pygame.image.load('imgs/3.png')
+    num_3.set_colorkey('white')
+    num_4 = pygame.image.load('imgs/4.png')
+    num_4.set_colorkey('white')
+    num_5 = pygame.image.load('imgs/5.png')
+    num_5.set_colorkey('white')
+    num_6 = pygame.image.load('imgs/6.png')
+    num_6.set_colorkey('white')
+    num_7 = pygame.image.load('imgs/7.png')
+    num_7.set_colorkey('white')
+    num_8 = pygame.image.load('imgs/8.png')
+    num_8.set_colorkey('white')
+    num_9 = pygame.image.load('imgs/9.png')
+    num_9.set_colorkey('white')
+    nums_dict = {
+        '0': num_0,
+        '1': num_1,
+        '2': num_2,
+        '3': num_3,
+        '4': num_4,
+        '5': num_5,
+        '6': num_6,
+        '7': num_7,
+        '8': num_8,
+        '9': num_9
+    }
+    HI_img = pygame.image.load('imgs/HI.png')
+    HI_img.set_colorkey('white')
+    mag_left = pygame.image.load('imgs/fire_magic_left.png')
+    mag_right = pygame.image.load('imgs/fire_magic_right.png')
+    dino1 = pygame.image.load('imgs/dino1.png')
+    dino1.set_colorkey('white')
+    road1 = pygame.image.load('imgs/road.png')
+    road1.set_colorkey('white')
+    dialog_left = pygame.image.load('imgs/dialog_left.png')
+    dialog_right = pygame.image.load('imgs/dialog_right.png')
+    d = Dino(100, 200)
+    running = True
+    mag_cord_x = WIDTH
+    mag_cord_y = 184
+    t = 1
+    t_c = 1
+    while running:
+
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                quit()  # running = False
+        screen.fill(color)
+        screen.blit(dino1, (d.x, d.y))
+        screen.blit(road1, (0, 270))
+        t += 1
+        if t < 200:
+            # t += 1
+            mag_cord_x -= 1
+        screen.blit(mag_left, (mag_cord_x, mag_cord_y))
+        if t > 200 and t < 300:
+            screen.blit(mag_left, (mag_cord_x, mag_cord_y))
+            screen.blit(dialog_right, (550, 400))
+            f2 = pygame.font.SysFont('arial', 14)
+            text = f2.render("Привет, Дино", False, (0, 0, 0))
+            screen.blit(text, (555, 405))
+            # t += 1
+        elif t >= 300 and t < 400:
+            screen.blit(mag_left, (mag_cord_x, mag_cord_y))
+            screen.blit(dialog_left, (100, 400))
+            f2 = pygame.font.SysFont('arial', 14)
+            text = f2.render("Здравствуйте? Вы очень похожи на мага воды.", False, (0, 0, 0))
+            screen.blit(text, (105, 405))
+            # t += 1
+        elif t >= 400 and t < 500:
+            screen.blit(mag_left, (mag_cord_x, mag_cord_y))
+            screen.blit(dialog_right, (550, 400))
+            f2 = pygame.font.SysFont('arial', 14)
+            text = f2.render("Я его брат. Как я понимаю он уже рассказал тебе про нашу ситуацию.", False, (0, 0, 0))
+            screen.blit(text, (555, 405))
+        elif t >= 500 and t < 600:
+            screen.blit(mag_left, (mag_cord_x, mag_cord_y))
+            screen.blit(dialog_left, (100, 400))
+            f2 = pygame.font.SysFont('arial', 14)
+            text = f2.render("Да", False, (0, 0, 0))
+            screen.blit(text, (105, 405))
+        elif t >= 600 and t < 700:
+            screen.blit(mag_left, (mag_cord_x, mag_cord_y))
+            screen.blit(dialog_right, (550, 400))
+            f2 = pygame.font.SysFont('arial', 14)
+            text = f2.render("Я помогу тебе. Сим. Салабим", False,
+                             (0, 0, 0))
+            screen.blit(text, (555, 405))
+        elif t >= 700 and t < 800:
+            screen.blit(mag_left, (mag_cord_x, mag_cord_y))
+            screen.blit(dialog_right, (550, 400))
+            f2 = pygame.font.SysFont('arial', 14)
+            text = f2.render("Теперь ты можешь извергать огненные шары. Они тебе помогут в битве с чудовищем.", False,
+                             (0, 0, 0))
+            screen.blit(text, (555, 405))
+        elif t >= 800 and t < 900:
+            screen.blit(mag_left, (mag_cord_x, mag_cord_y))
+            screen.blit(dialog_left, (100, 400))
+            f2 = pygame.font.SysFont('arial', 14)
+            text = f2.render("Спасибо. Я помогу вам. До свидания.", False, (0, 0, 0))
+            screen.blit(text, (105, 405))
+        elif t >= 900 and t < 1130:
+            # t += 1
+            mag_cord_x += 1
+            screen.blit(mag_right, (mag_cord_x, mag_cord_y))
+        elif t >= 1130:
+            running = False
+            origin_dino(screen, color, score, HI)
+
         score_out = []
         for i in range(1, len(score_str) + 1):
             score_out.insert(0, score_str[len(score_str) - i])
