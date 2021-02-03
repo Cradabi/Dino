640ff3f13f9d8597cf8102dd6fc22b14596bb83c
w-o-p <w-o-p@users.noreply.github.com>
2021-02-04 00:14:18 +0300

    buttons and tank images

--- menu.py
+++ menu.py
@@ -8,6 +8,28 @@ import pygame
 import os
 import sqlite3
 
+
+class Button:
+    def __init__(self, x, y, text, text_color):
+        self.x = x - 10
+        self.y = y - 10
+        self.w = text.get_width() + 20
+        self.h = text.get_height() + 20
+        self.border_width = 3
+        self.color = text_color
+        self.text = text
+
+    def draw(self, surf):
+        pygame.draw.rect(surf, self.color, (self.x, self.y, self.w, self.h), self.border_width)
+        screen.blit(self.text, (self.x + 10, self.y + 10))
+
+    def mouse_check(self, cort):
+        x, y = cort
+        if self.x <= x <= self.x + self.w and self.y <= y <= self.y + self.h:
+            return True
+        return False
+
+
 pygame.init()
 
 FPS = 60
@@ -38,6 +60,23 @@ pygame.mouse.set_cursor(*pygame.cursors.tri_left)
 language = 'rus'
 keys = [119, 115, 100, 97]  # 1-вверх, 2-вниз, 3-синий шар, 4-красный шар
 
+menu_color = (0, 0, 0)
+text_color = (255, 255, 255)
+
+image_tank1 = pygame.image.load('imgs/tank1.png')
+image_tank1.set_colorkey('white')
+image_tank2 = pygame.image.load('imgs/tank2.png')
+image_tank2.set_colorkey('white')
+
+if language == 'rus':
+    text1 = f1.render('Начать игру', True, text_color)
+    text2 = f1.render('Настройки', True, text_color)
+elif language == 'eng':
+    text1 = f1.render('Play', True, text_color)
+    text2 = f1.render('Settings', True, text_color)
+play_button = Button(100, 150, text1, text_color)
+settings_button = Button(110, 300, text2, text_color)
+
 # если надо до цикла отобразить объекты на экране
 screen.fill('black')
 
@@ -59,25 +98,40 @@ if __name__ == '__main__':
         # --------
         # изменение объектов и многое др.
         # --------
-        screen.fill('black')
+        screen.fill(menu_color)
 
-        text1 = f1.render('Начать игру', True, 'white')
-        screen.blit(text1, (100, 150))
-        text2 = f1.render('Настройки', True, 'white')
-        screen.blit(text2, (110, 300))
+        play_button.draw(screen)
+        settings_button.draw(screen)
 
         # t += 1
+        if play_button.mouse_check(pygame.mouse.get_pos()):
+            play_button.color = 'red'
+        else:
+            play_button.color = text_color
+
+        if settings_button.mouse_check(pygame.mouse.get_pos()):
+            settings_button.color = 'red'
+        else:
+            settings_button.color = text_color
 
         pressed = pygame.mouse.get_pressed()
         if pressed[0]:  # обработка нажатий левой кнопки мыши
             x1, y1 = pygame.mouse.get_pos()
-            if 90 <= x1 <= 360 and 140 <= y1 <= 210:
+            if play_button.mouse_check((x1, y1)):
                 pygame.mouse.set_visible(True)
                 origin_dino(screen, color, score, HI, birthday_code, language, keys, water_number,
                             fire_number, money)  # запуск игры
-            elif 100 <= x1 <= 350 and 290 <= y1 <= 360:
+            elif settings_button.mouse_check((x1, y1)):
                 pygame.mouse.set_visible(True)
                 birthday_code, score, language, keys = sets(screen, score, language, keys)  # настройки
+                if language == 'rus':
+                    text1 = f1.render('Начать игру', True, text_color)
+                    text2 = f1.render('Настройки', True, text_color)
+                elif language == 'eng':
+                    text1 = f1.render('Play', True, text_color)
+                    text2 = f1.render('Settings', True, text_color)
+                play_button = Button(100, 150, text1, text_color)
+                settings_button = Button(110, 300, text2, text_color)
 
         # обновление экрана
         pygame.display.update()
